ef46bb229e2dd291b9e72c07b3fb50971f609b8a
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2025-12-22 09:41:01 +0100

    Update Ko-Fi URL with username instead of ID

--- Packages/FoundationExtensions/Sources/FoundationExtensions/URL+TipJar.swift
+++ Packages/FoundationExtensions/Sources/FoundationExtensions/URL+TipJar.swift
@@ -3,10 +3,5 @@ import Foundation
 public extension URL {
 
     /// The Ko-fi donation page URL for supporting the application developer.
-    ///
-    /// This static URL provides a centralized reference to the Ko-fi donation page,
-    /// used throughout the application wherever the Tip Jar or donation links are displayed.
-    /// The URL points to the developer's Ko-fi profile where users can make one-time
-    /// donations to support the application development.
-    static let tipJarURL = URL(string: "https://ko-fi.com/Z8Z0C9KPT")!
+    static let tipJarURL = URL(string: "https://ko-fi.com/otaviocc")!
 }
